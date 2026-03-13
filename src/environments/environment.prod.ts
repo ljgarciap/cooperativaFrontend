f165cdf949771ac8e78051099d94e9f02e96188b
Luis Garcia <lujogarpin78@gmail.com>
2026-03-12 22:39:37 -0500

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.tudominio.com/api',
-  storageUrl: 'https://api.tudominio.com'
+  apiUrl: 'https://apicooperativa.softclass.co/api',
+  storageUrl: 'https://apicooperativa.softclass.co'
 };
